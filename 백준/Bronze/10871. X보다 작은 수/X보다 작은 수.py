3d58c05697126d96489223107d80de03f78bfe85
lizardnote <112800025+lizardnote@users.noreply.github.com>
2023-02-09 16:08:57 +0900

    [Bronze V] Title: X보다 작은 수, Time: 48 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,6 +1,5 @@
 import sys
 a, b = map(int, sys.stdin.readline().split())
 data = list(map(int, sys.stdin.readline().split()))
-
-filtered = list(filter(lambda x : x < b, data))
+filtered = [x for x in data if x < b]
 print(*filtered)
