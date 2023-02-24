bfaeca2cbd9c2b14f9f7dab0cb66a90f2bbac6f5
lizardnote <112800025+lizardnote@users.noreply.github.com>
2023-02-24 23:35:49 +0900

    [Silver V] Title: 너의 평점은, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,11 @@
+dic  = {'A+':4.5, 'A0':4.0, 'B+':3.5, 'B0':3.0, 'C+':2.5, 'C0':2.0, 'D+':1.5, 'D0':1.0, 'F':0.0}
+score = 0
+total = 0
+
+for _ in range(20):
+    s,p,g = input().split()
+    p = float(p)
+    if g != 'P' :
+        score = score + (dic[g] * p)
+        total += p
+print('%.6f' % (score/total))
