c504690a12faa36d2211567aaca3a6265b9dfd89
lizardnote <112800025+lizardnote@users.noreply.github.com>
2023-02-27 22:23:09 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+
+A = [list(map(int, input().split())) for _ in range(n)]
+B = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        print(A[i][j] + B[i][j], end = ' ')
+    print() 
+       
