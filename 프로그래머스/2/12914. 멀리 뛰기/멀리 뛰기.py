765c0315462d21027d23895ae67394ed376d63b0
lizardnote <112800025+lizardnote@users.noreply.github.com>
2024-09-20 17:04:04 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.88 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = 0
+    check = {}
+    check[1] = 1
+    check[2] = 2
+    
+    for i in range(3,n+1):
+        check[i] = check[i-2] + check[i-1]  
+    answer = check[n] % 1234567
+    return answer
