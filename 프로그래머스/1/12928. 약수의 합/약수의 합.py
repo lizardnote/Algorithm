7491355516c1297f8a4daf976f289ab9e92984b7
lizardnote <112800025+lizardnote@users.noreply.github.com>
2025-02-22 10:55:07 +0900

    [level 1] Title: 약수의 합, Time: 0.22 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,7 +1,7 @@
 def solution(n):
-    num_list= []
-    for i in range(1,n+1) :
-        if n % i ==0 :
-            num_list.append(i)
+    answer = 0
+    for i in range(1,n+1):
+        if n % i == 0:
+            answer += i
     
-    return sum(num_list)
+    return answer
