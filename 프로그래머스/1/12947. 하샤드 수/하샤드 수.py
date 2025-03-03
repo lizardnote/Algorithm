257fd1b420bb527220be5080292510226116f0da
lizardnote <112800025+lizardnote@users.noreply.github.com>
2025-03-03 21:40:00 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
@@ -1,11 +1,6 @@
 def solution(x):
-    
-    x_li = [int(i) for i in str(x)]
-    print(x_li)
-    if x % sum(x_li) == 0:
+    answer = False
+    if x % sum([int(x) for x in str(x)]) == 0 :
         answer = True
-    else:
-        answer = False
-        
-    return answer
     
+    return answer
