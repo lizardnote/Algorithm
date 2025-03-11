6d12414e7604be0c6935573c25156a70afc664ae
lizardnote <112800025+lizardnote@users.noreply.github.com>
2025-03-12 07:02:24 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.33 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -1,6 +1,4 @@
 def solution(price, money, count):
-    tot = 0
     for i in range(1,count+1):
-        tot += (price*i)
-
-    return tot-money if tot >= money else 0
+        money -= price*i
+    return -1*money if money < 0 else 0
