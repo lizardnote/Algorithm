c490d609f1d653383ef2bac9cf44e998205f1522
lizardnote <112800025+lizardnote@users.noreply.github.com>
2025-03-12 06:48:14 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.90 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,13 +1,9 @@
 def solution(left, right):
-    answer = 0
-    
-    for num in range(left,right+1):
-        
-        div_list = [i for i in range(1,num+1) if num % i == 0]
-       
-        if len(div_list) % 2 == 0:
-            answer += num
-        else:
-            answer -= num
-
-    return answer
+    # li = []
+    # for i in range(left,right+1):
+    #     if i % (i**(1/2)) == 0 :
+    #         li.append(i*-1)
+    #     else:
+    #         li.append(i)
+    # return sum(li)
+    return sum([(-i if i%(i**(1/2)) == 0 else i) for i in range(left, right+1)])
