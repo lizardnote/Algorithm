2babc6ae87343427ee37a8a75e3b51f8af972a25
lizardnote <112800025+lizardnote@users.noreply.github.com>
2025-03-18 07:37:14 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 5.01 ms, Memory: 9.19 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -0,0 +1,10 @@
+def solution(numbers):
+    answer = []
+    
+    for i in range(len(numbers)):
+        for j in range(i+1,len(numbers)):
+            k = numbers[i]+numbers[j]
+            if k not in answer:
+                answer.append(k)
+                
+    return sorted(answer)
