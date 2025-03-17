f867bb0f8a4ca1036196d5fe6bb67d6e7de6893b
lizardnote <112800025+lizardnote@users.noreply.github.com>
2025-03-18 08:31:22 +0900

    [level 1] Title: 모의고사, Time: 2.42 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,19 +1,30 @@
 def solution(answers):
-    answer = [0,0,0]
-    ans = []
+    answer = [0] * 3
     num1 = [1,2,3,4,5]
     num2 = [2,1,2,3,2,4,2,5]
     num3 = [3,3,1,1,2,2,4,4,5,5]
     
     for i in range(len(answers)):
-        if num1[i%5] == answers[i] :
+        if answers[i] == num1[i%5]:
             answer[0] += 1
-        if num2[i%8] == answers[i] :
+        if answers[i] == num2[i%8]:
             answer[1] += 1
-        if num3[i%10] == answers[i]:
+        if answers[i] == num3[i%10]:
             answer[2] += 1
+            
+    winner = max(answer)
+    person= []
+    for i in range(len(answer)):
+        if answer[i] == winner:
+            person.append(i+1)
+            
+    return person
+        
+        
+        
+
     
-    for i in range(3):
-        if answer[i] == max(answer):
-            ans.append(i+1)    
-    return ans
+    
+#     answer = [cnt1, cnt2, cnt3]
+    
+#     return max(answer)
